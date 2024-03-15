312dce9c75c51d0510ca14b5c741e4b13bd3a0b0
Primodial002 <115640591+Primodial002@users.noreply.github.com>
2024-03-15 17:08:00 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -26,7 +26,7 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
                                           message_id=file_id)
         elif Config.FORWARD_AS_COPY is False:
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                              message_id=file_id)
+                                              message_ids=file_id)
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return media_forward(bot, user_id, file_id)
